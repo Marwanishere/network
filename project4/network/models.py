417584bb3ef2a9747fcb8459c828f6dad185eda5
Marwanishere <marwan.hasan.work@gmail.com>
2023-12-19 12:45:38 +0000

    attempting to get the network to show old posts, so far there are no crashes however posts are not yet displayed even though they can be submitted

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class NewPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "user1")
+    title = models.CharField(max_length=64)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
--- project4/network/urls.py
+++ project4/network/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("getPosts", views.call_posts, name = "call_posts")
 ]
