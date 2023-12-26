4a6aa4edca917f2c26b567d46e0bd9342309f577
Marwanishere <marwan.hasan.work@gmail.com>
2023-12-26 23:04:38 +0000

    the follow button is run and console.log shows who the intended recipient of the follow request is however the innertext of the follow button does not change depending on the click of the follow button.

---
+++ project4/network/static/network/smprofile.js
@@ -0,0 +1,62 @@
+// function below acquired from cs50 chatbot and is purely there to solve a techincal issue 403 error
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+// below query selector acquired by cs50 chatbot
+document.querySelectorAll('#delete-button').forEach(button => {
+    button.addEventListener('click', delete_old_post);
+});
+// below function skeleton/body made using cs50 chatbot then adapted to fit use case
+function delete_old_post(e) {
+    e.preventDefault()
+    let id = e.target.dataset.id;
+    console.log(`Deleted post with id ${id}`)
+    fetch(`/delete_post/${id}/`, {
+        method: 'DELETE',
+        headers: {
+            // line below used in conjunction with getCookie function to solve techical issue, acquired 
+            // through cs50 chatbot prompting
+            'X-CSRFToken': getCookie('csrftoken'),
+        },
+        body: JSON.stringify({
+            id: id
+        })
+    })
+    .finally(()=> location.reload());
+}
+document.querySelectorAll('#follow_button').forEach(button => {
+    button.addEventListener('click', toggle_follow_unfollow);
+});
+function toggle_follow_unfollow(e){
+    e.preventDefault()
+    let username = e.target.dataset.username;
+    console.log(`following ${username}`) 
+    // the above line is returning following undefined
+    let follow_buttonjs = document.getElementById("follow_button");
+    fetch(`/smprofile/${username}/`, {
+        method: 'GET'
+        })
+    .then (response => response.json())
+    .then (data =>{
+        if (data.followstatus == true) {
+            followstatus = false
+            follow_buttonjs.innerText = "Unfollow"
+        }
+        else {
+            followstatus = true
+            follow_buttonjs.innerText = "Follow"
+        }
+    })
+    // .finally(()=> location.reload());
+}
